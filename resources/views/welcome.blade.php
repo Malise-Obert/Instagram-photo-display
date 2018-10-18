ef91adf581a383bd0e87bba8e4cf975fae302c81
Malise Obert <malise.obert@gmail.com>
2018-10-18 13:43:29 +0200

    Modified the blade template to display the posts pictures pulled from Instagram

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,31 +65,21 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+        <div class="container app">
+            <div class="posts col-xs-12 col-sm-12 col-md-8 col-lg-6">
+                @if(count($posts))
+                    @foreach($posts as $post)
+                        <div class="post">
+                            @if($post->type === 'image')
+                                <div class="image">
+                                    <img src="{{$post->images->standard_resolution->url}}" alt="">
+                                </div>
+                            @else
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                            @endif
+                        </div>
+                    @endforeach
+                @endif
             </div>
         </div>
     </body>
